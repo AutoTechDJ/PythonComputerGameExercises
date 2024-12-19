b7dc15283a8e6e7c1455b17db3d594db572cfc01
AutoTechDJ <johnsondarrius13@hotmail.com>
2024-12-18 23:47:33 -0500

    Starting process of game build

---
+++ DarriusGame.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+def choosePath()
+    path = ''
+    while path != 'left' and path !='right':
+        print('Choose your path wisely ^_^ (left or right)')
+        path = input()
+
